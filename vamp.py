867f4d0156a16423d9d608a4aec6214c73d65f5a
xrander <olamideadu@gmail.com>
2024-03-06 10:34:14 +0100

    if, elif, and else, flow statement - file vamp.py

---
+++ vamp.py
@@ -0,0 +1,12 @@
+name = "Carol"
+age = 3000
+
+if name == "Alice":
+    print("Hi Alice")
+elif age < 17:
+    print("You are not Alice Kiddo")
+elif age > 1000:
+    print("Are you a vampire or something?")
+    print("Unlike you, Alice is not an undead. Immortal vampire")
+elif age > 100:
+    print("You are not Alice. You are a grannie")
