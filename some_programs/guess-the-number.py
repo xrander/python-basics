53dce12e4281dfc85e1d7a398fdba809b80877c1
xrander <olamideadu@gmail.com>
2024-03-09 20:15:51 +0100

    stable version of guess-the-number.py, intro print added - line 5, number of guesses added, line 24 - 25, success or trial exhausted message added, line 28 to 33

--- some_programs/guess-the-number.py
+++ some_programs/guess-the-number.py
@@ -1,14 +1,16 @@
 # This is a simple guess the number games
 
 import random
-rand_number = random.randint(1, 30)
+
+print("This is a simple number guessing game, and you have 5 trials.")
+rand_number = random.randint(1, 30) # Generate random numbers
 print("I am thinking of a number between 1 and 30")
 
 user_guesses = 0
 
 while True:
     print("Take a guess: ")
-    user_number = int(input())
+    user_number = int(input()) # Get the user input
 
     user_guesses += 1
 
@@ -17,6 +19,15 @@ while True:
     elif user_number > rand_number:
         print("Your guess is high. Try again")
     else:
+        # Correct guess, break out of loop
         break
-print(f"Good job! the number is {rand_number}.")
-print(f"You got my guess in {user_guesses} guesses.")
+    if user_guesses == 5:
+        break # stop guesses after 10 times
+
+
+if user_number == rand_number:
+    # Since guess is correct, we inform the user
+    print(f"Good job! the number is {rand_number}.")
+    print(f"You got my guess in {user_guesses} guesses.")
+else:
+    print(f"Nice try, you have exhausted the number of trials, I was expecting {rand_number}")
